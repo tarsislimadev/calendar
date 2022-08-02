a9c6fcb5fb8d9347222c41738cfd1b893648f1db
Tarsis de Lima <br.tmvdl@gmail.com>
2022-08-02 07:56:18 -0300

    Adding commons.js file

---
+++ app/commons.js
@@ -0,0 +1,17 @@
+
+nTag.fromElement(document.body)
+  .setStyle('margin', '0')
+  .setStyle('padding', '0')
+  .setStyle('font-size', '16px')
+  .setStyle('font-family', 'sans-serif')
+  .setStyle('background-color', '#eeeeee')
+
+const title = new nText()
+
+title.setText('Agenda')
+title.setStyle('color', '#ffffff')
+title.setStyle('padding', '1rem 0')
+title.setStyle('font-weight', 'bold')
+title.setStyle('font-size', '2.5rem')
+title.setStyle('text-align', 'center')
+title.setStyle('background-color', '#000099')
