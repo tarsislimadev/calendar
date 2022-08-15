7f58e4695158041b728e839a2080d101a939ce34
Tarsis de Lima <br.tmvdl@gmail.com>
2022-08-15 16:19:57 -0300

    Fixing environment scripts

--- env/down.sh
+++ env/down.sh
@@ -1,1 +1,16 @@
+echo
+echo Docker stop
+echo
 docker-compose -f $(pwd)/docker-compose.yml down --remove-orphans --rmi all 
+
+echo
+echo Empty removing
+echo
+sudo rm -rf $( find -empty )
+sudo rm -rf $( find -empty )
+sudo rm -rf $( find -empty )
+
+echo
+echo Data removing
+echo
+sudo rm -rf ./data/ 
