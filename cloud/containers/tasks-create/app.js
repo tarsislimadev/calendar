cbf69212a3fe76afe1a6c59d997ed112641bbe8f
Tarsis de Lima <br.tmvdl@gmail.com>
2022-08-16 20:34:21 -0300

    Fixing '/tasks/create' endpoint

--- cloud/containers/tasks-create/app.js
+++ cloud/containers/tasks-create/app.js
@@ -1,11 +1,11 @@
 const tasksIndex = require('/agenda/commons/db').in('tasks')
 
 module.exports = ({ body }, res) => {
-  const { name, where, who, start_date, end_date, why } = body
+  const { where, who, start_date, end_date, why } = body
   const created_at = Date.now().toString()
 
   const task = tasksIndex.new()
-  task.writeMany({ name, where, who, start_date, end_date, why, created_at })
+  task.writeMany({ where, who, start_date, end_date, why, created_at })
 
   return res.json({ id: task.getId(), created_at })
 }
