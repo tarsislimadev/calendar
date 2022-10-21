81b3595eccc6f68bb56e2dea9990370b26b37754
Tarsis de Lima <br.tmvdl@gmail.com>
2022-10-21 16:40:08 -0300

    Fixing source paths

--- src/www/html/js/nTag.js
+++ src/www/html/js/nTag.js
@@ -232,6 +232,7 @@ class nLabel extends nTag {
       component: { name: 'label' },
     })
 
+    this.setStyle('margin-bottom', '0.5rem')
     this.setStyle('padding-top', '0.5rem')
     this.setStyle('padding-botton', '0.5rem')
   }
