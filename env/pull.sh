984f6ba13f515be94ce5d85b13cedbeb6b00f547
Tarsis de Lima <br.tmvdl@gmail.com>
2022-10-23 17:35:02 -0300

    Fixing PULL script

--- env/pull.sh
+++ env/pull.sh
@@ -2,6 +2,8 @@ git clone git@github.com:brtmvdl/calendario-front.git src/www
 
 git clone git@github.com:brtmvdl/main-node-file.git src/main
 
+git clone git@github.com:brtmvdl/taskscreate-node-file.git src/taskscreate
+
 git clone git@github.com:brtmvdl/tasksget-node-file.git src/tasksget
 
 git clone git@github.com:brtmvdl/taskslist-node-file.git src/taskslist
