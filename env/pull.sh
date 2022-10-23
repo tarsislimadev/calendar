8b56d3515d61521fb0808be7527ca289460057d8
Tarsis de Lima <br.tmvdl@gmail.com>
2022-10-23 17:22:10 -0300

    Fixing PULL script

--- env/pull.sh
+++ env/pull.sh
@@ -1,7 +1,7 @@
-git submodule add git@github.com:brtmvdl/main-node-file.git src/main
+git clone git@github.com:brtmvdl/main-node-file.git src/main
 
-git submodule add git@github.com:brtmvdl/tasksget-node-file.git src/tasksget
+git clone git@github.com:brtmvdl/tasksget-node-file.git src/tasksget
 
-git submodule add git@github.com:brtmvdl/taskslist-node-file.git src/taskslist
+git clone git@github.com:brtmvdl/taskslist-node-file.git src/taskslist
 
-git submodule add git@github.com:brtmvdl/tasksupdate-node-file.git src/tasksupdate
+# git clone git@github.com:brtmvdl/tasksupdate-node-file.git src/tasksupdate
