c7203989a9eeb2986030dfa834f1a263d7e15bce
Tarsis de Lima <br.tmvdl@gmail.com>
2022-10-23 18:17:12 -0300

    Removing useless files

--- old/tasks-list/app.js
+++
@@ -1,3 +0,0 @@
-const tasksIndex = require('/calendario/commons/db').in('tasks')
-
-module.exports = (_, res) => res.json({ list: tasksIndex.listJSON() })
--- old/tasks-update/app.js
+++
@@ -1,9 +0,0 @@
-const tasksIndex = require('/calendario/commons/db').in('tasks')
-
-module.exports = ({ body: { _id, where, who, start_date, end_date, why } }, res) => {
-  const updated_at = Date.now().toString()
-
-  const task = tasksIndex.selectById(_id)
-  task.writeMany({ where, who, start_date, end_date, why, updated_at })
-  return res.json({ id: task.getId(), updated_at })
-}
