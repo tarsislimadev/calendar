6694951eb773bb4482c0867392535f2ed6a15dba
Tarsis <br.tmvdl@gmail.com>
2022-12-21 10:29:16 -0300

    Fixing INSTALL script

--- env/install.sh
+++ env/install.sh
@@ -1,3 +1,3 @@
-docker-compose -f docker-compose.yaml app npm i
+docker-compose -f ./docker-compose.yaml run app npm i
 
-docker-compose -f docker-compose.yaml www npm i
+docker-compose -f ./docker-compose.yaml run www npm i
