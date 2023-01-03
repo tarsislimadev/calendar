e8c608487505efd58913b97a94e9f4e9552a0ff9
Tarsis <br.tmvdl@gmail.com>
2023-01-03 19:07:47 -0300

    Fixing scripts

--- env/pull.sh
+++ env/pull.sh
@@ -1,11 +1,3 @@
 git clone git@github.com:brtmvdl/calendario-front.git src/www
 
 git clone git@github.com:brtmvdl/main-node-file.git src/main
-
-git clone git@github.com:brtmvdl/taskscreate-node-file.git src/taskscreate
-
-git clone git@github.com:brtmvdl/tasksget-node-file.git src/tasksget
-
-git clone git@github.com:brtmvdl/taskslist-node-file.git src/taskslist
-
-git clone git@github.com:brtmvdl/tasksupdate-node-file.git src/tasksupdate
